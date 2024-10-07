37a2f0b9a7f30651fa7ff47d0f63c4ac9edd7de9
Christian Asch <christian.asch4@gmail.com>
2024-10-07 14:34:31 -0600

    Updated example

--- class4/saxpy_sm.c
+++ class4/saxpy_sm.c
@@ -1,17 +1,16 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <omp.h>
-void saxpy(float a, float * x, float * y, const int elements, const int chunk_size)
-{
-#pragma omp parallel for schedule(static, chunk_size) default(none) shared(a, x, y, chunk_size, elements)
-  for(int i = 0; i < elements; ++i)
-  {
+void saxpy(float a, float *x, float *y, const int elements,
+           const int chunk_size) {
+#pragma omp parallel for schedule(static, chunk_size) default(none)            \
+    shared(a, x, y, chunk_size, elements)
+  for (int i = 0; i < elements; ++i) {
     y[i] = a * x[i] + y[i];
   }
 }
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
   double start, end;
   int total_elements = atoi(argv[1]);
   int chunk_size = atoi(argv[2]);
@@ -21,9 +20,9 @@ int main(int argc, char ** argv)
   x = malloc(sizeof(float) * total_elements);
   y = malloc(sizeof(float) * total_elements);
 
-#pragma omp parallel for schedule(static, chunk_size) default(none) shared(total_elements, x, y, chunk_size)
-  for(int i = 0; i < total_elements; ++i)
-  {
+#pragma omp parallel for schedule(static, chunk_size) default(none)            \
+    shared(total_elements, x, y, chunk_size)
+  for (int i = 0; i < total_elements; ++i) {
     x[i] = 1.f;
     y[i] = 2.3f;
   }
